7df428f55afc5e9e0ba4bfeb4d2db0a14cbcd013
jesse samuel widjaja <jessewidjaja@gmail.com>
2023-05-09 17:39:34 -0700

    footer

--- frontend/src/components/main/Footer.tsx
+++ frontend/src/components/main/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => {
 	return (
 		<footer className="bg-primary text-gray-300">
-			<div className="flex justify-between items-center py-6">
+			<div className="flex justify-between items-center py-12 px-8">
 				<div className="flex space-x-4 text-white">
 					<Link href="/about">About</Link>
 					<Link href="/contact">Contact Us</Link>
--- frontend/src/components/main/NavBar.tsx
+++ frontend/src/components/main/NavBar.tsx
@@ -19,8 +19,13 @@ const NavBar = () => {
 						Opportunities
 					</Link>
 				</li>
-				<li className="bg-white text-blue-700 py-2 px-4 rounded-full font-bold hover:bg-blue-100">
-					<Link href="#">List Your Apt</Link>
+				<li>
+					<Link
+						href="#"
+						className="text-white mx-4 hover:text-gray-300"
+					>
+						List Your Apt
+					</Link>
 				</li>
 			</ul>
 		</nav>
