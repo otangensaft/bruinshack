27bf13b8fd3b69078607a02b65a2612615eeac88
jesse samuel widjaja <jessewidjaja@gmail.com>
2023-05-15 20:34:20 -0700

    optimistic updates

--- frontend/src/api/apartments/createApartments.ts
+++ frontend/src/api/apartments/createApartments.ts
@@ -4,15 +4,41 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 const queryClient = useQueryClient();
 
 const createApartments = data => {
-	return axios.post("/planets", data);
+	return axios.post("/apartment", data);
 };
 
 export const useCreateApartments = () => {
 	return useMutation({
-		mutationFn: createApartments,
-		onSuccess: () => {
+		onMutate: async newApartment => {
 			// @ts-ignore
-			queryClient.invalidateQueries({ queryKey: ["planets"] });
+			await queryClient.cancelQueries("apartments");
+
+			// @ts-ignore
+			const previousApartments = queryClient.getQueryData("apartments");
+
+			// @ts-ignore
+			queryClient.setQueryData("apartments", [
+				// @ts-ignore
+				...(previousApartments || []),
+				newApartment.data,
+			]);
+
+			// @ts-ignore
+			return { previousApartments };
 		},
+		onError: (_, __, context) => {
+			if (context.previousApartments) {
+				queryClient.setQueryData(
+					// @ts-ignore
+					"apartments",
+					context.previousApartments,
+				);
+			}
+		},
+		onSettled: () => {
+			// @ts-ignore
+			queryClient.invalidateQueries({ queryKey: ["apartments"] });
+		},
+		mutationFn: createApartments,
 	});
 };
