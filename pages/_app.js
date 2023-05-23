47785c67d0c2c8d41ec866ba6ff1a6c40d8d3cdd
Danny Tang <tang_daniel@outlook.com>
2023-05-22 23:26:43 -0700

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,47 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { useEffect } from "react";
+
+const App = ({ Component, pageProps, router }) => {
+
+  // Used to Handle Google Analytics
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", "G-2H2KGMVMSS", {
+        page_path: url,
+      });
+    };
+
+    // Subscribe to the change event
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return(
+    <>
+
+      {/* Google Analytics Stuff.  Questions? Watch This: https://www.youtube.com/watch?v=m7oQSA3c31Q*/}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-2H2KGMVMSS"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-2H2KGMVMSS');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    
+    </>
+  )
 }
 
-export default MyApp
+export default App
